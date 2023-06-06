cfa2432c3be1da9532dbdf802251abf94d02cf7c
Jonathan Galdino <johnzballad@gmail.com>
2023-06-06 04:11:29 -0300

    fix: we don't want to quit when running tests

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,5 +1,7 @@
+const isTestEnvironment = process.env.NODE_ENV === 'test'
+
 export default function error(message: string, exit: boolean) {
   console.error(message)
 
-  exit && process.exit(1)
+  exit && !isTestEnvironment && process.exit(1)
 }
